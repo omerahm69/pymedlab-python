be4a85df6fcd7a74f4dffec5329bed58b4a987e3
omerahm69 <153000625+omerahm69@users.noreply.github.com>
2026-08-28 19:38:57 +0200

    Create 02_data_types.py

---
+++ module-02-python-fundamentals/02_data_types.py
@@ -0,0 +1,10 @@
+modality = "CT"
+number_of_slices = 300
+slice_thickness = 1.25
+is_contrast_used = True
+
+
+print(type(modality))
+print(type(number_of_slices))
+print(type(slice_thickness))
+print(type(is_contrast_used))
