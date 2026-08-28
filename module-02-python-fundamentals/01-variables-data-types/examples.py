2c371dc21b87b73ba0242fa7f9a9164c109cbfb9
omerahm69 <153000625+omerahm69@users.noreply.github.com>
2026-08-28 19:35:01 +0200

    Add patient metrics and calculate BMI and total dose

--- module-02-python-fundamentals/01-variables-data-types/examples.py
+++ module-02-python-fundamentals/01-variables-data-types/examples.py
@@ -23,3 +23,27 @@ slice_thickness = 1.0
 print(patient_name)
 print(age)
 print(slice_thickness)
+
+patient_age = 45
+patient_height = 1.72
+patient_weight = 74
+
+print(patient_age)
+print(patient_height)
+print(patient_weight)
+
+patient_age = 45
+patient_height = 1.72
+patient_weight = 74
+
+bmi = patient_weight / patient_height**2
+
+print("BMI:", bmi)
+
+
+dose = 2.5
+number_of_fractions = 20
+
+total_dose = dose * number_of_fractions
+
+print("Total dose:", total_dose, "Gy")
